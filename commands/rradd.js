471dada6a745224c72a523c0a5f04a0576c6f30f
DarkBoy <71411869+Darkboy-js@users.noreply.github.com>
2020-10-30 16:49:28 +0300

    Update rradd.js

--- commands/rradd.js
+++ commands/rradd.js
@@ -4,7 +4,7 @@
 
  module.exports = {
     name: "rradd",
-    description: "add reaction role",
+    description: "set auto-partner channel",
     run: async (client, message, args, db, prefix) => {
     if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(`**YOU MUST HAVE PERMISSIONS.**`)
     let channel = message.mentions.channels.first();
@@ -20,9 +20,13 @@
     let check = message.guild.roles.cache.find(r => r.name === `${role.name}`)
     if(!check) return message.channel.send(`invaild role!`)
     if(!args[3]) return message.channel.send(`${prefix}rradd ${channel} ${args[1]} ${role.name} <EMOJI> `)
-    let customemoji = Discord.Util.parseEmoji(args[3]);
+    function isCustomEmoji(emoji) {
+        return emoji.split(":").length == 1 ? false : true;
+      }
+      if (isCustomEmoji(args[3])) {
+      let customemoji = Discord.Util.parseEmoji(args[3]);
     let emojicheck = client.emojis.cache.find(emoji => emoji.id === `${customemoji.id}`);
-    if(!emojicheck) return message.channel.send(`this emoji is invaild! (You Cant use discord default emotes)`)
+    if(!emojicheck) return message.channel.send(`this emoji is invaild!`)
   let embed = new Discord.MessageEmbed()
  .setThumbnail(message.guild.iconURL())
  .setTitle(`${yes} Reaction Role Sucsses!`)
@@ -39,12 +43,37 @@
     message.channel.send(embed)
      client.channels.cache.get(`${channel.id}`).messages.fetch(`${args[1]}`).then(a => {
          a.react(emojicheck.id)
-     db.set(`rrremove_${message.guild.id}_${args[1]}2`, channel.id)
+      db.set(`rrremove_${message.guild.id}_${args[1]}2`, channel.id)
      db.set(`rrremove_${message.guild.id}_${args[1]}_${args[3]}`, emojicheck.id)
-     db.set(`rerremove_${message.guild.id}_${args[1]}`, args[1])
+      db.set(`rerremove_${message.guild.id}_${args[1]}`, args[1])
      db.set(`emoteid_${message.guild.id}_${emojicheck.id}`, emojicheck.id)
      db.set(`role_${message.guild.id}_${emojicheck.id}`, role.id)
      db.set(`message_${message.guild.id}_${emojicheck.id}`, args[1])
-         })
-
+     return;    
+    })
+         return;
+    }
+      db.set(`rrremove_${message.guild.id}_${args[1]}2`, channel.id)
+     db.set(`rrremove_${message.guild.id}_${args[1]}_${args[3]}`, args[3])
+     db.set(`rerremove_${message.guild.id}_${args[1]}`, args[1])
+     db.set(`emoteid_${message.guild.id}_${args[3]}`, args[3])
+     db.set(`role_${message.guild.id}_${args[3]}`, role.id)
+     db.set(`message_${message.guild.id}_${args[3]}`, args[1])
+     let embed = new Discord.MessageEmbed()
+     .setThumbnail(message.guild.iconURL())
+     .setTitle(`${yes} Reaction Role Sucsses!`)
+     .setDescription(`**Done!**
+     
+     **${msg} [Go To Message](https://discord.com/channels/${message.guild.id}/${channel.id}/${args[1]})
+     ${mention} Role : ${role}
+     ${attention} Emoji: ${args[3]}
+     ${channele} Channel : ${channel}**
+     `)
+     .setTimestamp()
+     .setFooter(message.guild.name , message.guild.iconURL())
+    
+        message.channel.send(embed)
+         client.channels.cache.get(`${channel.id}`).messages.fetch(`${args[1]}`).then(a => {
+             a.react(args[3])
+         })    
     }}
